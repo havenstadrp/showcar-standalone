32576acdfe2287d3faebca544004505ae81bcefa
Danish Khan <61810778+PLOKMJNB@users.noreply.github.com>
2020-11-04 14:26:09 -0500

    new font

--- main.lua
+++ main.lua
@@ -93,24 +93,23 @@ AddEventHandler('onResourceStop', function(res)
     end
 end)
 
-function Draw3DText(x, y, z, textInput, fontId, scaleX, scaleY)
-	local px, py, pz = table.unpack(GetGameplayCamCoords())
-	local dist       = GetDistanceBetweenCoords(px, py, pz, x, y, z, 1)    
-	local scale      = (1 / dist) * 20
-	local fov        = (1 / GetGameplayCamFov()) * 100
-	local scale      = scale * fov   
-	SetTextScale(scaleX * scale, scaleY * scale)
-	SetTextFont(fontId)
-	SetTextProportional(1)
-	SetTextColour(250, 250, 250, 255)
-	SetTextDropshadow(1, 1, 1, 1, 255)
-	SetTextEdge(2, 0, 0, 0, 150)
-	SetTextOutline()
-	SetTextEntry("STRING")
-	SetTextCentre(1)
-	AddTextComponentString(textInput)
-	SetDrawOrigin(x, y, z + 2, 0)
-	DrawText(0.0, 0.0)
-	ClearDrawOrigin()
+function Draw3DText(x, y, z, textInput, scaleX, scaleY)
+    local px, py, pz = table.unpack(GetGameplayCamCoords())
+    local dist       = GetDistanceBetweenCoords(px, py, pz, x, y, z, 1)    
+    local scale      = (1 / dist) * 20
+    local fov        = (1 / GetGameplayCamFov()) * 100
+    local scale      = scale * fov   
+    SetTextScale(scaleX * scale, scaleY * scale)
+    SetTextFont("FontName")
+    SetTextProportional(1)
+    SetTextColour(250, 250, 250, 255)
+    SetTextDropshadow(1, 1, 1, 1, 255)
+    SetTextEdge(2, 0, 0, 0, 150)
+    SetTextOutline()
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(textInput)
+    SetDrawOrigin(x, y, z + 2, 0)
+    DrawText(0.0, 0.0)
+    ClearDrawOrigin()
 end
-
